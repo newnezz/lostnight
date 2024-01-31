792b4724c465066affee337ca7dea6d908b2f806
Michael Nunez <michael@Michaels-Laptop.local>
2024-01-31 01:15:09 -0600

    1.0.7 changed story a bit to be more coherent to the main narrative, finished first choice more completely

--- index.js
+++ index.js
@@ -2,38 +2,48 @@
 const getInput = require('./input');
 const clearConsole = require('./clear')
 
-let alive = true;
-let good = 5;
+let fear;
+let self;
+let envy;
 
 clearConsole();
 console.log('******************');
-console.log('*** Willoughby ***');
+console.log('*** Lost Night ***');
 console.log('******************');
 
 async function main() {
     console.log(`
-    You hear a knock at your door.
-    It's 1AM and aren't expecting anyone.
+    - 1AM -
+    You can't fall asleep.
+    Not that you don't want to.
+    Something in you simply won't allow it.
+    At least not yet.
+    `)
+    await getInput('')
+
+    console.log(`
+    Someone knocks at the door.
+    I'm not expecting anyone..
     You wait, hoping they leave.
     `)
-
     await getInput('')
 
     console.log(`
     But then, another knock. This time harder.
-    The discomfort increases, and you try hard to remain still.
+    Your discomfort increases, but try to remain still.
+    Surely they will leave.
     `)
 
     await getInput('')
 
     console.log(`
     "Please, help me..."
-    The voice sounds frail, yet distorted.
+    The voice sounds familiar, yet distorted.
     Maybe it's the lady across the hall.
     She's been quite sick lately.
     `)
 
-    // First Choice
+    // First Choice > Fear
     let answer;
     while (answer !== 'y' && answer != 'n') {
         answer = await getInput('Answer the door? (y / n)')
@@ -41,21 +51,23 @@ async function main() {
     switch (answer) {
         case 'y':
             console.log(`You answer the door and find no one there.`)
-            good++;
+            fear = false;
             break;
         case 'n':
             console.log(`You ignore the knocking.`)
-            good--;
+            fear = true;
             break;
         default:
             break;
     }
 
+    await getInput('')
+
     console.log('\n')
     console.log(`
     That was odd, but either way the knocking is over.
     You decide to try and get some sleep. 
-    Maybe this time it will be different..
+    Maybe they will finally let me.
     `)
 
     await getInput('')
@@ -63,15 +75,24 @@ async function main() {
     console.log('\n')
     console.log(`
     - 3AM -
-    You wake up to another noise.
-    This time the creaking of a window.
-    As you get up, you notice the front door is open.
-    Did I forget to lock it again?
+    Hours later, sleep has not come.
+    ${fear ? 
+        `Creaking of a door startles you.
+         Looking around you notice the front door open.
+         Did I forget to lock it?`
+         : ''
+    }
     `)
 
     await getInput('')
     console.log(`
-    While closing the door, you glimpse across the hall
+    You decide to take a walk.
+    Maybe some fresh air will help.
+    `)
+
+    await getInput('')
+    console.log(`
+    While leaving, you glimpse across the hall
     and notice a sign on the neighbors door.
     "ESTATE SALE: MONDAY JULY 9TH, EVERYTHING MUST GO"
     `)
@@ -90,31 +111,36 @@ async function main() {
     He looks down at the floor, avoiding eye contact.
     `)
 
-    // Second Choice
+    // Second Choice > Self
     let answer2;
     while (answer2 !== 'y' && answer2 != 'n') {
         answer2 = await getInput('Say something? (y / n)')
     }
     switch (answer2) {
         case 'y':
-            console.log(`
-            You attempt to make small conversation,
-            but the man doesn't respond. Though you
-            can see a slight change to his demeanor.
-            `)
-            good++;
+    console.log(`
+    You attempt to make small conversation,
+    but the man doesn't respond. Though you
+    can see a slight change to his demeanor.
+    `)
+            self = false;
             break;
         case 'n':
-            console.log(`
-            There is little use speaking to him. He
-            seems to want to be left alone, and Id rather
-            focus on getting back to sleep.
-            `)
-            good--;
+    console.log(`
+    There is little use speaking to him. He
+    seems to want to be left alone, and Id rather
+    focus on getting back to sleep.
+    `)
+            self = true;
             break;
         default:
             break;
     }
+
+    await getInput('')
+    console.log(`
+    
+    `)
 }
 
 main();
